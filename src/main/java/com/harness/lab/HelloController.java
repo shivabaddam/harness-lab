5117a0317bc72287d85bc70d888d8c2ce9d9b286
Shruthi Vindhayala <Shruthi.Vindhayala@rrtelematics.com>
2026-03-19 10:08:53 -0500

    Adds app name

--- src/main/java/com/harness/lab/HelloController.java
+++ src/main/java/com/harness/lab/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Harness CI/CD Kubernetes Lab!";
+        return "Harness-lab: Hello from Harness CI/CD Kubernetes Lab!";
     }
 
     @GetMapping("/health")
